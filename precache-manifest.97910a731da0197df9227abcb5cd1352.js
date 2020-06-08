61fa94cfaaeffd5404f24b376831cda9588aefc1
llsanketll <sanket.lamsal@gmail.com>
2020-06-08 21:52:11 +0545

    Updates

---
+++ precache-manifest.97910a731da0197df9227abcb5cd1352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d2bd4db1af0b20899e1c2816498885",
+    "url": "/Random-Pokemon/index.html"
+  },
+  {
+    "revision": "5038f873eb866e69c5d1",
+    "url": "/Random-Pokemon/static/css/main.594b0b49.chunk.css"
+  },
+  {
+    "revision": "5fc6ccd4ce7cc3fecc7a",
+    "url": "/Random-Pokemon/static/js/2.30bd0e30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Pokemon/static/js/2.30bd0e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5038f873eb866e69c5d1",
+    "url": "/Random-Pokemon/static/js/main.d3952223.chunk.js"
+  },
+  {
+    "revision": "7c24d455e7991943deba",
+    "url": "/Random-Pokemon/static/js/runtime-main.e1bf927c.js"
+  }
+]);
